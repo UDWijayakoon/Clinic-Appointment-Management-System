f821342664debb6d4f13636cf40b72817b8496f2
UDWijayakoon <udewminiw@gmail.com>
2026-05-15 22:37:46 +0530

    Added configuration files

---
+++ ClinicAppointmentAPI/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+let connectDB = async () => {
+
+    try {
+
+        await mongoose.connect(process.env.MONGO_URL);
+
+        console.log("MongoDB Connected");
+
+    } catch (error) {
+
+        console.log(error);
+
+    }
+
+};
+
+export default connectDB;
